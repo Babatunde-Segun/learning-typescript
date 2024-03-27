ad7522ed74d42bdcd85f3854e64107ac65ca7f71
Babatunde-Segun <officiallyvincify@gmail.com>
2024-03-28 00:40:55 +0100

    initial commit on typescript

---
+++ with-typescript.js
@@ -0,0 +1,6 @@
+var n = function (a, b) {
+    return a + b;
+};
+var i = n(3, 5);
+console.log(i);
+// console.log(reslt);
---
+++ with-typescript.ts
@@ -0,0 +1,7 @@
+const n = function (a: number, b: number) {
+  return a + b;
+};
+
+const i = n(3, 5);
+console.log(i);
+// console.log(reslt);
